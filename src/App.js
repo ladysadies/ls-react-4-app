c3c4332590f46272e621670ff247c72f7d8bb2ee
LadySadies <ladysadies@anniecannons.com>
2021-05-15 01:04:53 -0700

    added unicorn eating potato chips header

--- src/App.js
+++ src/App.js
@@ -3,9 +3,13 @@ import './App.css'
 import DisplayedRecipes from "./components/DisplayedRecipes"
 import NewRecipe from "./components/NewRecipe"
 
+
 const App = () => {
     return (
       <div className="primary-div">
+          <div className="header">
+
+          </div>
           <h1>React Recipe App</h1>
           < NewRecipe />
           < DisplayedRecipes />
